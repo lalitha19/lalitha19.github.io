88c27cd366fcc689a90ecca32966bcbd66b2d92f
Lalitha Pasupatheeswaran <lalitha@tishansoft.in>
2022-02-24 23:49:47 +0530

    recipe model included

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Recipe } from '../recipe.model';
 @Component({
   selector: 'app-recipes-list',
   templateUrl: './recipes-list.component.html',
   styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent implements OnInit {
-
+recipes:Recipe[]=[
+  new Recipe('Test Recipe','recipe description','https://static01.nyt.com/images/2021/06/01/dining/11lightveg-roundup-8/11lightveg-roundup-8-articleLarge-v2.jpg')
+];
   constructor() { }
 
   ngOnInit(): void {
