efedd7184c7e406ddcd09b72ee2dcbf9e1165f4b
Lalitha Pasupatheeswaran <lalitha@tishansoft.in>
2022-03-01 00:10:32 +0530

    Data Biniding

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+
 import { Recipe } from '../recipe.model';
 @Component({
   selector: 'app-recipes-list',
@@ -6,6 +7,8 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent implements OnInit {
+  
+@Output() recipeWasSelected=new EventEmitter<Recipe>();
 recipes:Recipe[]=[
   new Recipe('Test Recipe','recipe description','https://static01.nyt.com/images/2021/06/01/dining/11lightveg-roundup-8/11lightveg-roundup-8-articleLarge-v2.jpg')
 ];
@@ -13,5 +16,11 @@ recipes:Recipe[]=[
 
   ngOnInit(): void {
   }
-
+  onRecipeSelected(recipe:Recipe){
+;this.recipeWasSelected.emit(recipe)
+  }
 }
+function Ouput() {
+  throw new Error('Function not implemented.');
+}
+
--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
+import { Recipe } from './recipe.model';
 
 @Component({
   selector: 'app-recipes',
@@ -6,10 +8,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes.component.css']
 })
 export class RecipesComponent implements OnInit {
-
+  selectedRecipe!:Recipe;
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  
 
 }
