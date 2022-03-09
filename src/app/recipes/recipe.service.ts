fa3c096639ac8761cbc1ef12857f858b18be7a54
Lalitha Pasupatheeswaran <lalitha@tishansoft.in>
2022-03-09 22:55:03 +0530

    Route

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,14 +1,26 @@
-import { EventEmitter } from "@angular/core";
+import { EventEmitter, Injectable } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
+import { ShoppingListService } from "../shopping-list/shopping-list.service";
 import { Recipe } from "./recipe.model";
 
+@Injectable()
 export class RecipeService{
 
     recipeSelected=new EventEmitter<Recipe>();
    private recipes:Recipe[]=[
-        new Recipe('Test Recipe','recipe description','https://static01.nyt.com/images/2021/06/01/dining/11lightveg-roundup-8/11lightveg-roundup-8-articleLarge-v2.jpg')
+        new Recipe('Test Recipe','recipe description','https://static01.nyt.com/images/2021/06/01/dining/11lightveg-roundup-8/11lightveg-roundup-8-articleLarge-v2.jpg',
+        [new Ingredient('bun',2),new Ingredient('bread',5)]
+        )
       ];
+
+      constructor   (private slService:ShoppingListService){}
 
       getRecipes(){
           return this.recipes;
+      }
+
+      addIngredientsToShoppingList(ingredients:Ingredient[]){
+          this.slService.addIngredients(ingredients);
+
       }
 }
